10f38394db739992ff3d8c335648482197c293c1
alhad-tm <alhadchikkutm@gmail.com>
2023-09-28 10:58:11 +0530

    added routers *****

--- src/App.js
+++ src/App.js
@@ -1,39 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
-import Main from './components/Main/Main';
-import Header from './components/Header/Header';
-import YProgressbar from './components/YProgressbar/YProgressbar';
-import Cards from './components/Cards/Cards';
-import Register from './components/Register/Register';
-import Countdown from './components/Countdown/Countdown';
+import "./App.css";
+import YProgressbar from "./components/YProgressbar/YProgressbar";
+import Register from "./components/Register/Register";
+import Countdown from "./components/Countdown/Countdown";
+import Thankyou from "./components/Thankyou/Thankyou";
+import Checkout from "./components/Checkout/Checkout";
+import PageNotFound from "./components/PageNotFound/PageNotFound";
+import Footer from "./components/Footer/Footer";
 
-import Thankyou from './components/Thankyou/Thankyou';
-import About from './components/About/About';
-import MemberProgress from './components/MemberProgress/MemberProgress';
-import Checkout from './components/Checkout/Checkout';
-import PageNotFound from './components/PageNotFound/PageNotFound';
-import Footer from './components/Footer/Footer';
-import RegistrationClosed from './components/RegistrationClosed/RegistrationClosed';
-import Profile from './components/Profile/Profile';
+import RegistrationClosed from "./components/RegistrationClosed/RegistrationClosed";
+import Profile from "./components/Profile/Profile";
+import Home from "./components/Home/Home";
+import Header from "./components/Header/Header";
+
+import { BrowserRouter as Router , Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Header/>
-     <Main/>
-     <MemberProgress/>
-     <Cards/>
-     <About/>
+      <Router>
+
+      <Header />
+     <Routes>
+      <Route path="/" exact element={<Home />}/>
+      <Route path="/register" element={  <Register />}/>
+      <Route path="/countdown" element={  <Countdown />}/>
+      <Route path="/checkout" element={  <Checkout />}/>
+      <Route path="/thankyou" element={     <Thankyou />}/>
+      <Route path="/registrationclosed" element={  <RegistrationClosed />}/>
+      <Route path="/profile" element={  <Profile/>}/>
+      <Route path="/not" element={  <PageNotFound />}/>
+      
+      </Routes>
+      
+       
+      <YProgressbar />
+    
    
-     <YProgressbar/>
-     <Register/>
-     <Countdown/> 
-     <Thankyou/>
-     <Checkout/> 
-     <PageNotFound/>
-     <RegistrationClosed/>
-     <Profile/>
-     <Footer/>
+   
+    
+    
+    
+     
+      <Footer />
+      </Router>
     </div>
   );
 }
---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+import Main from '../Main/Main'
+import MemberProgress from '../MemberProgress/MemberProgress'
+import Cards from '../Cards/Cards'
+import About from '../About/About'
+
+const Home = () => {
+  return (
+    <div>
+      
+        <Main/>
+        <MemberProgress/>
+        <Cards/>
+        <About/>
+      
+    </div>
+  )
+}
+
+export default Home
