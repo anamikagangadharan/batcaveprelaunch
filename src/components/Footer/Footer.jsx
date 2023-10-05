4e6056a7b171a81fa8d629c3a5060a75b7ba14fe
alhad-tm <alhadchikkutm@gmail.com>
2023-10-05 10:28:11 +0530

    changed phn to whatsapp and added link

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import css from "./Footer.module.css"
 import Insta from "../../assets/instagram.svg"
-import Phone from "../../assets/phone.svg"
+// import Phone from "../../assets/phone.svg"
+import Whatsapp from "../../assets/whatsapp.svg"
 import Mail from "../../assets/mail.svg"
 
 const Footer = () => {
+
+
+  const phoneNumber = '+919786378480'; // Replace with the desired phone number
+  const message = 'Hello, this is a test message!'; // Replace with your message
+
+  const whatsappUrl = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
+
   return (
     <div className={css.container}>
         <div className={css.wrap}>
@@ -19,11 +27,12 @@ const Footer = () => {
         <div className={css.right}> 
         <a href="https://www.instagram.com/batcave.in/"  target="_blank" rel="noopener noreferrer">    <img src={Insta} alt="" /> </a>
            
-            <div className={css.tooltipcontainer}>
+            {/* <div className={css.tooltipcontainer}>
             <img onclick="window.open('tel:+91 8877880101');" className={css.phone} src={Phone} alt="" />
             <span className={css.tooltip}>+91 8877880101</span>
-            </div>
-           
+            </div> */}
+             <a href={whatsappUrl} target="_blank" rel="noopener noreferrer">
+           <img src={Whatsapp} alt="" /> </a>
 
             <a href="mailto: connect@invicious.in">    <img src={Mail} alt="" /> </a>
 
